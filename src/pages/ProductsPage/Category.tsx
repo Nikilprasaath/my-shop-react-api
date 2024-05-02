e68d3397b999f5dc367bc11b99bcbb0888ec6cd2
ndhanara <nikilprasaath2001@gmail.com>
2024-05-02 11:52:48 +0530

    product page

---
+++ src/pages/ProductsPage/Category.tsx
@@ -0,0 +1,59 @@
+import ICategory from "../../models/ICategories";
+
+const Category: React.FC<ICategory> = (props) => {
+
+  return (
+    // <div>
+    //   <nav>
+    //     <div>
+    //       <div>
+    //         Categories
+    //       </div>
+      
+    //       <div>
+    //         <ul className="nav flex-column">
+    //           <li className="nav item">
+    //             <Link className="nav-link active" aria-current="page" to="/">
+    //               All
+    //             </Link>
+    //           </li>
+    //           <li className="nav item">
+    //             <Link
+    //               className="nav-link"
+    //               aria-current="page"
+    //               to="/products"
+    //             >
+    //               Men
+    //             </Link>
+    //           </li>
+    //           <li className="nav item">
+    //             <Link
+    //               className="nav-link"
+    //               aria-current="page"
+    //               to="/contact"
+    //             >
+    //               Women
+    //             </Link>
+    //           </li>
+    //           <li className="nav item">
+    //             <Link
+    //               className="nav-link"
+    //               aria-current="page"
+    //               to="/about"
+    //             >
+    //               Kids
+    //             </Link>
+    //           </li>
+    //         </ul>
+    //       </div>
+    //     </div>
+    //   </nav>
+    // </div>
+    <div>
+      
+
+    </div>
+  )
+}
+
+export default Category
--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -1,22 +1,99 @@
 import axios, { AxiosResponse } from "axios";
-import Categories from "./CategoriesList";
-import ProductList from "./ProductsList";
+import Category from "./Category";
+import ProductComponent from "../../components/ProductComponent";
 import { ProductService } from "../../services/ProductService";
+import { useEffect, useState } from "react";
+import IProduct from "../../models/IProduct";
+import ICategory from "../../models/ICategories";
+import { Link } from "react-router-dom";
 
-const ProductsPage = () => {
-  let data = new ProductService()
-  console.log(data.getProducts())
-  
+const ProductsPage: React.FC = () => {
+  const [Categories, setCategories] = useState([]);
+  const [Products, setProducts] = useState([]);
+
+  const sortByList = []
+
+  const getCategoryDetails = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/categories`);
+      setCategories(response.data);
+    } catch (error) {
+      // Handle errors
+      console.log(error);
+    }
+  };
+
+  const getProductDetails = async (category?: string) => {
+    try {
+      const response = await axios.get(`http://localhost:3000/products`);
+      setProducts(response.data);
+    } catch (error) {
+      // Handle errors
+      console.log(error);
+    }
+  };
+
+  const onSelectCategories = async (category: string) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/products?category=${category}`
+      );
+      console.log(response.data);
+      setProducts(response.data);
+    } catch (error) {
+      // Handle errors
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCategoryDetails();
+    getProductDetails();
+  }, []);
 
   return (
     <div className="container text-center">
       <div className="row">
         <div className="col-md-1">
-          <Categories />
+          <h4>Category</h4>
+          <div>
+            <ul className="nav flex-column">
+              <div>
+                <li className="nav item">
+                  <Link to="" onClick={() => getProductDetails()}>
+                    All
+                  </Link>
+                </li>
+              </div>
+              {Categories.map((category: ICategory, index) => (
+                <div>
+                  <li className="nav item">
+                    <Link
+                      to=""
+                      onClick={() => onSelectCategories(category.name)}
+                    >
+                      {category.name}
+                    </Link>
+                  </li>
+                </div>
+              ))}
+            </ul>
+          </div>
         </div>
         <div className="col">
-          <ProductList />
-        </div> 
+          <div className="row">
+            {Products.map((product: IProduct, index) => (
+              <div className="col-md-4">
+                <ProductComponent
+                  key={product.id}
+                  title={product.title}
+                  imgSrc={product.imgSrc}
+                  maxRetailPrice={product.maxRetailPrice}
+                ></ProductComponent>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
